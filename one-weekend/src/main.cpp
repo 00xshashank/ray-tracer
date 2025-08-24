4f498107167a825c8c0596b671b88a04581db35f
00xshashank <shashank.krishnamani@gmail.com>
2025-08-24 16:59:14 +0530

    Reorganize files

---
+++ one-weekend/src/main.cpp
@@ -0,0 +1,59 @@
+#include <fstream>
+
+#include "vec3.h"
+#include "ray.h"
+#include "color.h"
+
+colour ray_colour(const ray& y) {
+    return colour(0, 0, 0);
+}
+
+int main() {
+    auto aspect_ratio = 16.0/9.0;
+    int image_width = 400;
+
+    int image_height = (image_width) / aspect_ratio;
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left - 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    std::ofstream file("image.ppm");
+
+    if (!file) {
+        std::cerr << "Error opening file" << std::endl;
+        return 1;
+    }
+
+    file << "P3\n" << image_height << ' ' << image_width << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+
+            ray r(camera_center, ray_direction);
+            colour pixel_colour = ray_colour(r);
+            write_colour(file, pixel_colour);
+        }
+    }
+
+    file.close();
+
+    std::clog << "\rDone.                                              \n";
+
+    return 0;
+}
